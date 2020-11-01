7933d89644caa3e7bba91b88ce8fb7ec2b66ffe3
AdrianKonca <48868811+AdrianKonca@users.noreply.github.com>
2020-11-01 22:12:54 +0100

    Adds popcorn as the main route;

--- popcorn/urls.py
+++ popcorn/urls.py
@@ -8,7 +8,7 @@ Function views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home') 
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('popcorn.urls')),
-    path('admin/', admin.site.urls),
+        #path('', 'main'),
+        path('accounts/', include('django.contrib.auth.urls')),
 ]
